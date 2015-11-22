4e9c3314dbca64359935ed12dea3529f30b02865
triestpa <patrick.triest@gmail.com>
2015-11-21 20:23:15 -0500

    Remove unessesary files

--- MagicLightsServer/public/js/app.js
+++ MagicLightsServer/public/js/app.js
@@ -1,1 +1,1 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+angular.module('magicLightsApp', ['ngRoute', 'ngMaterial', 'appRoutes', 'MainCtrl', 'LightsService']);
--- MagicLightsServer/public/js/controllers/MainCtrl.js
+++ MagicLightsServer/public/js/controllers/MainCtrl.js
@@ -1,5 +1,12 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, lights) {
 
-	$scope.tagline = 'To the moon and back!';	
+	$scope.red = "255000000";
+	$scope.green = "000255000";
+	$scope.blue = "000000255";
 
+
+	$scope.colorClick = function(color) {
+			console.log(color);
+			lights.changeColor({color: color});
+	};
 });
